ff9456519e32d21561f626b917735e9f0d65fa94
Renato Guimaraes Nunes <renatognunes@hotmail.com>
2019-09-30 14:07:22 -0400

    login page

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import withStyles from "@material-ui/core/styles/withStyles";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 // Material UI
 import Card from "@material-ui/core/Card";
@@ -24,6 +26,8 @@ const styles = {
 
 class Post extends Component {
   render() {
+    // Format data dayjs package
+    dayjs.extend(relativeTime);
     const { classes } = this.props;
     const {
       body,
@@ -52,7 +56,7 @@ class Post extends Component {
             {userHandle}
           </Typography>
           <Typography variant="body2" color="textSecondary">
-            {createdAt}
+            {dayjs(createdAt).fromNow()}
           </Typography>
           <Typography variant="body1">{body}</Typography>
         </CardContent>
