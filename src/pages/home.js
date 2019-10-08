13e0c79ee62c3e052e1d4350bd37974573aaa54e
Renato Guimaraes Nunes <renatognunes@hotmail.com>
2019-10-07 20:38:37 -0400

    Data Reducer

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,29 +1,24 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import axios from "axios";
 import Grid from "@material-ui/core/Grid";
+
+import { connect } from "react-redux";
+import { getPosts } from "../redux/actions/dataActions";
 
 import Post from "../components/Post";
 import Profile from "../components/Profile";
 
 class Home extends Component {
-  state = {
-    posts: null
-  };
-
   componentDidMount() {
-    axios
-      .get("/posts")
-      .then(res => {
-        this.setState({
-          posts: res.data
-        });
-      })
-      .catch(err => console.error(err));
+    this.props.getPosts();
   }
 
   render() {
-    let recentPostsMarkup = this.state.posts ? (
-      this.state.posts.map(post => <Post key={post.postId} post={post} />)
+    const { posts, loading } = this.props.data;
+
+    let recentPostsMarkup = !loading ? (
+      posts.map(post => <Post key={post.postId} post={post} />)
     ) : (
       <p>Loading...</p>
     );
@@ -41,4 +36,20 @@ class Home extends Component {
   }
 }
 
-export default Home;
+Home.propTypes = {
+  getPosts: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  data: state.data
+});
+
+const mapActionsToProps = {
+  getPosts
+};
+
+export default connect(
+  mapStateToProps,
+  mapActionsToProps
+)(Home);
